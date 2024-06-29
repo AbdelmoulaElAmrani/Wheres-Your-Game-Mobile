60f5597d5f1dc4cb3df8db93bab13690a834977a
Dofamen <elouakifi.mehdi@gmail.com>
2024-06-29 19:03:46 +0100

    small fix

--- appTimersConfig.ts
+++ appTimersConfig.ts
@@ -2,4 +2,7 @@
 export const CHAT_REFRESH_TIMER: number = 10;
 export const CONVERSATION_REFRESH_TIMER: number = 15;
 export const NOTIFICATION_REFRESH_TIMER: number = 30;
+export const googleWebClientId: string = "798054162153-5ceu8fakl487r4n7vvltapdubbuag6g6.apps.googleusercontent.com";
+export const googleIosClientId: string = "798054162153-e99bjqq6709aqhfe922ab9fhl7nv3f7q.apps.googleusercontent.com";
+export const googleAndroidClientId: string = "798054162153-lvbugkqi5jvmsb1evnttt5bn6esir3bi.apps.googleusercontent.com";
 export const API_URI = `https://sport-app-38dd22818116.herokuapp.com/api/`;
