39d13dce2215a6d02c20b15f69d43318f001ca46
Anasse <anassedemnati@gmail.com>
2024-11-14 23:35:10 +0100

    devs

--- app/(user)/(search)/SearchUser.tsx
+++ app/(user)/(search)/SearchUser.tsx
@@ -1,18 +1,19 @@
-import {Alert, FlatList, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { Alert, FlatList, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import CustomNavigationHeader from "@/components/CustomNavigationHeader";
-import {ImageBackground} from "expo-image";
-import {router, useLocalSearchParams} from "expo-router";
-import React, {useCallback, useEffect, useState} from "react";
-import {Avatar, Searchbar} from "react-native-paper";
-import {heightPercentageToDP, widthPercentageToDP} from "react-native-responsive-screen";
+import { ImageBackground } from "expo-image";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useCallback, useEffect, useState } from "react";
+import { Avatar, Searchbar } from "react-native-paper";
+import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
 import Spinner from "@/components/Spinner";
-import {UserService} from "@/services/UserService";
-import {UserResponse} from "@/models/responseObjects/UserResponse";
+import { UserService } from "@/services/UserService";
+import { UserResponse } from "@/models/responseObjects/UserResponse";
 import UserType from "@/models/UserType";
-import {Ionicons} from "@expo/vector-icons";
-import {useSelector} from "react-redux";
-import {FriendRequestService} from "@/services/FriendRequestService";
+import { Ionicons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
+import { FriendRequestService } from "@/services/FriendRequestService";
+import { ChildrenService } from "@/services/ChildrenService";
 
 
 const SearchUser = () => {
@@ -21,7 +22,8 @@ const SearchUser = () => {
     const currentUser = useSelector((state: any) => state.user.userData) as UserResponse;
     const [searchType, setSearchType] = useState<UserType>();
     const params = useLocalSearchParams<any>();
-
+    const [searchName, setSearchName] = useState<string>('');
+    const [people, setPeople] = useState<UserSearchResponse[]>([]);
 
     useEffect(() => {
         const param = params?.searchType as keyof typeof UserType | undefined;
@@ -42,8 +44,7 @@ const SearchUser = () => {
             router.back();
     }
 
-    const [searchName, setSearchName] = useState<string>('');
-    const [people, setPeople] = useState<UserSearchResponse[]>([]);
+
 
     const _onSearchSubmit = useCallback(async () => {
         if (searchName.trim() === '') {
@@ -71,7 +72,7 @@ const SearchUser = () => {
         setPeople(oldPeople => {
             return oldPeople.map(person => {
                 if (person.id === receiverId) {
-                    return {...person, friend: true};
+                    return { ...person, friend: true };
                 }
                 return person;
             });
@@ -79,35 +80,37 @@ const SearchUser = () => {
     };
 
     const _onSendingParentingRequest = async (receiverId: string) => {
-        //TODO:: make sure to alert a message to the user that the invite has been set successfully
         try {
-            const senderId = currentUser.id;
-            //const d = await FriendRequestService.sendParentingRequest(senderId, receiverId);
+            const res = await ChildrenService.sendParentRequest(receiverId);
+            if (!res) {
+                Alert.alert('Error', 'Failed to send parenting request. Please try again.');
+                return;
+            }
             Alert.alert(
                 'Invitation Sent',
                 'Your parenting request has been sent successfully.',
-                [{text: 'OK', onPress: () => console.log('Alert closed')}]
+                [{ text: 'OK', onPress: () => console.log('Alert closed') }]
             );
-            /*setPeople(oldPeople => {
-                        return oldPeople.map(person => {
-                            if (person.id === receiverId) {
-                                return {...person, friend: true};
-                            }
-                            return person;
-                        });
-                    });*/
+            setPeople(oldPeople => {
+                return oldPeople.map(person => {
+                    if (person.id === receiverId) {
+                        return { ...person, parent: true };
+                    }
+                    return person;
+                });
+            });
         } catch (error) {
             console.error('Error sending parenting request:', error);
             Alert.alert('Error', 'Failed to send parenting request. Please try again.');
         }
     }
 
-    const _renderUserItem = ({item}: { item: UserSearchResponse }) => {
+    const _renderUserItem = ({ item }: { item: UserSearchResponse }) => {
         return (
             <TouchableOpacity style={styles.userItem}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                     {item.imageUrl ? (
-                        <Avatar.Image size={35} source={{uri: item.imageUrl}}/>
+                        <Avatar.Image size={35} source={{ uri: item.imageUrl }} />
                     ) : (
                         <Avatar.Text
                             size={35}
@@ -116,12 +119,12 @@ const SearchUser = () => {
                     )}
                     <Text style={styles.userName}>{`${item.firstName} ${item.lastName}`}</Text>
                 </View>
-                {(!item.friend || (!item.parent && isParenting)) ?
+                {(!item.friend && (!item.parent && isParenting)) ?
                     <Ionicons
                         onPress={() => isParenting ? _onSendingParentingRequest(item.id) : _onAddFriendOrRemove(item.id)}
-                        name="person-add-outline" size={20} color="black"/>
+                        name="person-add-outline" size={20} color="black" />
                     :
-                    <Ionicons name="person-sharp" size={20} color="#2757CB"/>
+                    <Ionicons name="person-sharp" size={20} color="#2757CB" />
                 }
             </TouchableOpacity>
         );
@@ -137,7 +140,7 @@ const SearchUser = () => {
             source={require('../../../assets/images/signupBackGround.jpg')}>
             <SafeAreaView>
                 {loading && (
-                    <Spinner visible={loading}/>
+                    <Spinner visible={loading} />
                 )}
                 <CustomNavigationHeader
                     text={isParenting ? 'Add a Child' : `Search ${searchType ? UserType[searchType] : ''}`}
@@ -160,16 +163,16 @@ const SearchUser = () => {
                 </View>
                 <View style={styles.mainContainer}>
                     <KeyboardAvoidingView
-                        style={{width: '100%', height: '100%', paddingVertical: 10, paddingHorizontal: 8, flex: 1}}>
-                        <View style={{width: '100%', marginBottom: 30, flex: 1}}>
+                        style={{ width: '100%', height: '100%', paddingVertical: 10, paddingHorizontal: 8, flex: 1 }}>
+                        <View style={{ width: '100%', marginBottom: 30, flex: 1 }}>
                             {people.length > 0 ? <FlatList
                                 data={people}
                                 keyExtractor={(item) => item.id.toString()}
                                 renderItem={_renderUserItem}
                                 style={styles.userList}
-                                ListFooterComponent={<View style={{height: heightPercentageToDP(80)}}/>}
+                                ListFooterComponent={<View style={{ height: heightPercentageToDP(80) }} />}
                             /> : <Text
-                                style={{textAlign: 'center', fontWeight: 'bold', marginTop: heightPercentageToDP(30)}}>No
+                                style={{ textAlign: 'center', fontWeight: 'bold', marginTop: heightPercentageToDP(30) }}>No
                                 User</Text>}
                         </View>
                     </KeyboardAvoidingView>
--- services/ChildrenService.ts
+++ services/ChildrenService.ts
@@ -1,17 +1,53 @@
+import { String } from './../node_modules/lightningcss/node/ast.d';
+import Requests from "./Requests";
+
+
 export class ChildrenService {
     static async getMyChildren() {
 
     }
 
     static async sendParentRequest(childId: string) {
+        try {
+            const res = await Requests.post(`parent-child/send-request?childId=${childId}`, {});
+            if (res?.status !== 200) {
+                return false;
+            }
+            return true;
+        } catch (e : any) {
+            return false;
+        }
 
     }
 
-    static async acceptParentRequest() {
+    static async acceptParentRequest(requestId : String) {
+        try {
+            console.log('in service' + requestId);
+            const res = await Requests.post(`parent-child/respond-request?requestId=${requestId}&accept=${true}`, {});
+            if (res?.status !== 200) {
+                return false;
+            }
+            return true;
+        }
+        catch (e : any) {
+            console.error(e);
+            return false;
+        }
 
     }
 
-    static async rejectParentRequest() {
+    static async rejectParentRequest(requestId : String) {
+        try {
+            const res = await Requests.post(`parent-child/respond-request?requestId=${requestId}&accept=false`, {});
+            if (res?.status !== 200) {
+                return false;
+            }
+            return true;
+        }
+        catch (e : any) {
+            return false;
+        }
 
     }
 }
+
